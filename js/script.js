a770f39ac5abdf6a94ac456402100385b64464bd
resource11 <mcmahon@resource11.com>
2015-10-12 08:52:56 -0400

    Tweaked style.css file, added some styling to boxes. Modified script.js. Exploring game logic and troubleshooting why game board pieces will change board text values yet will not recognize those text values as arguments for testing winner status.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 $(document).ready(function() {
 
 // finds each box in the grid
+  var winner = null;
   var boxGrid = $('#tic-tac-holder').children();
   var box = $('.box');
 
@@ -14,55 +15,140 @@ $(document).ready(function() {
 
 // play the game
 
-// check for winner
-var getWinner = function getWinner() {
-  if (winnerIs('X')) {
-    console.log('X');
-    return 'The winner is X';
-  }
-  else if (winnerIs('O')) {
-    console.log('O');
-    return 'The winner is O';
-  } else if (fullBoard()) {
-    return 'Cat\'s game';
-  } else {
-    return null;
-  }
-}
+// check for winner test
+// var getWinner = function getWinner() {
+//     if (winnerIs('X')) {
+//     console.log('X');
+//     return 'The winner is X';
+//   }
+//   else if (winnerIs('O')) {
+//     console.log('O');
+//     return 'The winner is O';
+//   } else {
+//     return null;
+//   }
+// };
 
-var winnerIs = function winnerIs(player) {
-  return winsRow(player) || winsColumn(player) || winsDiagonal(player);
-}
+// var winnerIs = function winnerIs(player) {
+//   return winsRow(player) || winsColumn(player) || winsDiagonal(player);
+// };
 
-var winsRow = function winsRow(player) {
-  return allThree(player, boxGrid[0], boxGrid[1], boxGrid[2]) ||
-         allThree(player, boxGrid[3], boxGrid[4], boxGrid[5]) ||
-         allThree(player, boxGrid[6], boxGrid[7], boxGrid[8]);
-}
+// var winsRow = function winsRow(player) {
+//   return allThree(player, boxGrid[0], boxGrid[1], boxGrid[2]) ||
+//          allThree(player, boxGrid[3], boxGrid[4], boxGrid[5]) ||
+//          allThree(player, boxGrid[6], boxGrid[7], boxGrid[8]);
+// };
 
-var winsColumn = function winsColumn(player) {
-  return allThree(player, boxGrid[0], boxGrid[3], boxGrid[6]) ||
-         allThree(player, boxGrid[1], boxGrid[4], boxGrid[7]) ||
-         allThree(player, boxGrid[2], boxGrid[5], boxGrid[8]);
-}
+// var winsColumn = function winsColumn(player) {
+//   return allThree(player, boxGrid[0], boxGrid[3], boxGrid[6]) ||
+//          allThree(player, boxGrid[1], boxGrid[4], boxGrid[7]) ||
+//          allThree(player, boxGrid[2], boxGrid[5], boxGrid[8]);
+// };
 
-var winsDiagonal = function winsDiagonal(player) {
-  return allThree(player, boxGrid[0], boxGrid[4], boxGrid[8]) ||
-         allThree(player, boxGrid[2], boxGrid[4], boxGrid[6]);
-}
+// var winsDiagonal = function winsDiagonal(player) {
+//   return allThree(player, boxGrid[0], boxGrid[4], boxGrid[8]) ||
+//          allThree(player, boxGrid[2], boxGrid[4], boxGrid[6]);
+// };
 
-var allThree = function allThree(player, boxOne, boxTwo, boxThree) {
-  return (boxOne === player) && (boxTwo === player) && (boxThree === player);
-}
+// var allThree = function allThree(player, boxOne, boxTwo, boxThree) {
+//   return (boxOne === player) && (boxTwo === player) && (boxThree === player);
+// };
+
+
+// check for winner long way
+var checkForWinner = function checkForWinner() {
+  if (
+  $(boxGrid[0]).text() && $(boxGrid[1]).text() && $(boxGrid[2]).text() === 'X' ||
+  $(boxGrid[3]).text() && $(boxGrid[4]).text() && $(boxGrid[5]).text() === 'X' ||
+  $(boxGrid[6]).text() && $(boxGrid[7]).text() && $(boxGrid[8]).text() === 'X' ||
+  $(boxGrid[0]).text() && $(boxGrid[3]).text() && $(boxGrid[6]).text() === 'X' ||
+  $(boxGrid[1]).text() && $(boxGrid[4]).text() && $(boxGrid[7]).text() === 'X' ||
+  $(boxGrid[2]).text() && $(boxGrid[5]).text() && $(boxGrid[8]).text() === 'X' ||
+  $(boxGrid[0]).text() && $(boxGrid[4]).text() && $(boxGrid[8]).text() === 'X' ||
+  $(boxGrid[2]).text() && $(boxGrid[4]).text() && $(boxGrid[6]).text() === 'X')
+
+  { winner = 'x';
+    console.log('winner is x'); } // else if (
+
+  // $(boxGrid[0]).text() && $(boxGrid[1]).text() && $(boxGrid[2]).text() === 'O' ||
+  // $(boxGrid[3]).text() && $(boxGrid[4]).text() && $(boxGrid[5]).text() === 'O' ||
+  // $(boxGrid[6]).text() && $(boxGrid[7]).text() && $(boxGrid[8]).text() === 'O' ||
+  // $(boxGrid[0]).text() && $(boxGrid[3]).text() && $(boxGrid[6]).text() === 'O' ||
+  // $(boxGrid[1]).text() && $(boxGrid[4]).text() && $(boxGrid[7]).text() === 'O' ||
+  // $(boxGrid[2]).text() && $(boxGrid[5]).text() && $(boxGrid[8]).text() === 'O' ||
+  // $(boxGrid[0]).text() && $(boxGrid[4]).text() && $(boxGrid[8]).text() === 'O' ||
+  // $(boxGrid[2]).text() && $(boxGrid[4]).text() && $(boxGrid[6]).text() === 'O')
+
+  // {
+  //   winner = 'o';
+  //   console.log('winner is o');
+  // }
+
+
+  // var winnerX = 'XXX';
+  // var winnerO = 'OOO';
+
+  // console.log('running test');
+  // var k = 0;
+  // var combo = '---';
+
+  // for (var i = 0; i < winCombo.length; i++) {
+  //   combo = winCombo[i];
+  //   console.log($(combo[i]).text());
+  // }
+  // if ((combo[0]+combo[1]+combo[2]) === 'XXX') {
+  //   console.log('winner is X');
+  // }
+  // if ((combo[0]+combo[1]+combo[2]) === 'OOO') {
+  //   console.log('winner is O');
+  // }
+
+
+  // for (var k in winCombo) {
+  //   var combo = winCombo[k];
+  //   console.log(winCombo[k]);
+  // }
+
+};
+
+
+// check for no dashes in board boxes
+// var isNotBlank = function isNotBlank(element, index, array) {
+//       return element !== '-';
+// }
+
+// var checkForBlanks = function checkForBlanks() {
+//   return boxGrid.every(isNotBlank);
+// }
 
 // check for full board with no winner
-var fullBoard = function fullBoard() {
-  if (!winnerIs('X') && !winnerIs('O') && ($(boxGrid).text() !== '---------')) {
-    // console.log($(boxGrid).text());
-    console.log('full board, no winner');
-    console.log($(boxGrid).text());
+// var fullBoard = function fullBoard() {
+//   // if (!winnerIs('X') || !winnerIs('O') || checkForBlanks) {
+//     // console.log($(boxGrid).text());
+//     for (var i = 0; i < boxGrid.length; i++) {
+//       if (boxGrid[i] !== '-') {
+
+//       }
+//     // }
+//     console.log('full board, no winner');
+//     console.log($(boxGrid).text());
+//   }
+// };
+ var checkForBlanks = function checkForBlanks() {
+  var boxCount = 0;
+  var i = 0;
+  for (i in this.boxGrid) {
+    if (boxGrid[i] !== '-') {
+      boxCount += 1;
+    }
+
+    if (boxCount === 9) {
+      return 'Cat\'s game';
+    }
   }
 };
+
+
 
 
 // if playerTurn is player01
@@ -73,11 +159,15 @@ var fullBoard = function fullBoard() {
   $(box).on('click', function() {
     if (player === player01 ){
           $(this).text('X');
-           getWinner($(this));
+          console.log(boxGrid.text());
+          // checkForBlanks();
+          checkForWinner();
           player = player02;
       } else {
           $(this).text('O');
-          getWinner($(this));
+          console.log(boxGrid.text());
+          // checkForBlanks();
+          checkForWinner();
           player = player01;
       }
   });
@@ -89,9 +179,6 @@ var fullBoard = function fullBoard() {
     player = player01;
   });
 
-  fullBoard();
-
-
 });
 
 // end scripts
@@ -99,77 +186,4 @@ var fullBoard = function fullBoard() {
 
 
 
-
-// 'use strict';
-
-// var init = function init() {
-//   var yourName = prompt('What is your name?');
-
-//   document.getElementById('name').textContent = yourName;
-
-//   var thingList = document.getElementById('fav-list');
-//   var newThingButton = document.getElementById('new-thing-button');
-//   var checkbox = document.getElementById('click-handler-box');
-//   var closeListButton = document.getElementById('close-list-button');
-
-//   var newThingButtonClickHandler = function newThingButtonClick(event) {
-//     event.preventDefault();
-//     MyApp.addToList(thingList);
-//   };
-
-//   var closeListButtonClickHandler = function closeListButtonClick(event) {
-//     newThingButton.disabled = !newThingButton.disabled;
-//     console.log(newThingButton.disabled);
-//     event.target.textContent = event.target.textContent === "No more things" ?
-//       "More things" :
-//       "No more things";
-//   };
-
-//   var liClickHandler = function liClick(event) {
-//     var text = event.target.textContent;
-//     alert(text);
-//   };
-
-//   var checkboxChangeHandler = function checkboxChange(event) {
-//     var listElements = thingList.children;
-//     // create varliable 'numListElements' and assign to it
-//     // the number of child elelments of our <ul> with id 'fav-list'
-//     var numListElements = thingList.childElementCount;
-
-//     if(event.target.checked) { // if the checkbox is checked
-//       //loop ove the length of the 'listElements' array
-//       for(var index=0; index < numListElements; index++) {
-//         // attach a click handler on each ,li> element in our <ul>
-//         listElements[index].addEventListener('click', liClickHandler);
-//       }
-//     } else { // if the checkbox is unchecked
-//       // loop over the length of the 'listElements' array
-//       for(var index=0; index < numListElements; index++) {
-//         // remove the specific click handler we set
-//         // on each <li> in our <ul>
-//         listElements[index].removeEventListener('click', liClickHandler);
-//       }
-//     }
-//   };
-
-//   newThingButton.addEventListener('click', newThingButtonClickHandler);
-//   closeListButton.addEventListener('click', closeListButtonClickHandler);
-//   checkbox.addEventListener('change', checkboxChangeHandler);
-// };
-
-// window.addEventListener('load', init);
-
-// // We can define things outside of the window.onload which are evaluated
-// // // only when called.
-// var MyApp = {};
-
-// MyApp.addToList = function(list) {
-//   var newLi = document.createElement('li');
-//   var newItemText = document.getElementById('new-thing');
-//   newLi.textContent = newItemText.value;
-//   newItemText.value = '';
-//   if (newLi.textContent !== '') {
-//     list.appendChild(newLi);
-//   }
-// };
 
